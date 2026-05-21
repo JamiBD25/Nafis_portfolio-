0a90590a33cf5b7d3c45ac56ae87f92b9aed0455
Nafis Mohammed Jami <nafismohammedjami@gmail.com>
2026-05-21 16:49:20 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementById('hamburger');
+
+const navLinks = document.getElementById('navLinks');
+
+hamburger.onclick = () => {
+
+navLinks.classList.toggle('active');
+
+};
